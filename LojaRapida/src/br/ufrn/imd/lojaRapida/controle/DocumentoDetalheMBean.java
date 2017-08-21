4914b47c1ffe39f7204576140d2eee5962db9b41
Jhoanes Freitas <jhoanesbarbosa@gmail.com>
2017-08-21 04:27:47 -0300

    Visualizando documentos

---
+++ LojaRapida/src/br/ufrn/imd/lojaRapida/controle/DocumentoDetalheMBean.java
@@ -0,0 +1,40 @@
+package br.ufrn.imd.lojaRapida.controle;
+
+import javax.faces.bean.ManagedBean;
+
+import br.ufrn.imd.lojaRapida.dao.DocumentoDAO;
+import br.ufrn.imd.lojaRapida.dominio.Documento;
+
+@ManagedBean
+public class DocumentoDetalheMBean {
+	
+	private Documento doc;
+	
+	public Documento getDoc() {
+		return doc;
+	}
+	
+	public void setDoc(Documento doc) {
+		this.doc = doc;
+	}
+	
+	public String selecionar(Documento doc) {
+		System.out.println(doc);
+		this.doc = doc;
+		return "/admin/documentoDetalhe";
+	}
+	
+	public String selecionar2(Documento doc) {
+		this.doc = doc;
+		return "/cliente/compra";
+	}
+	
+	public String selecionar3(Integer idDoc) {
+		System.out.println("Selecionar 3 - "+ doc);
+		DocumentoDAO dao = new DocumentoDAO();
+		this.doc = dao.findByPrimaryKey(idDoc);
+		dao.close();
+		return "/admin/documentoDetalhe";
+	}
+
+}
